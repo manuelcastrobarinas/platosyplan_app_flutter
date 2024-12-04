03d61db5d7078815aa053563fa85be08ccff6bce
manuel castro barinas <manuel.castrobarinas@gmail.com>
2024-12-04 09:58:27 -0500

    create login and user model

---
+++ lib/models/auth/auth.dart
@@ -0,0 +1,77 @@
+import 'dart:convert';
+
+class LoginResponse {
+  bool ok;
+  String message;
+  UserModel userModel;
+  String token;
+
+  LoginResponse({
+    required this.ok,
+    required this.message,
+    required this.userModel,
+    required this.token,
+  });
+
+  factory LoginResponse.fromRawJson(String str) => LoginResponse.fromJson(json.decode(str));
+
+  String toRawJson() => json.encode(toJson());
+
+  factory LoginResponse.fromJson(Map<String, dynamic> json) => LoginResponse(
+    ok        : json["ok"],
+    message   : json["message"],
+    userModel :UserModel.fromJson(json["user_model"]),
+    token     : json["token"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "ok": ok,
+    "message": message,
+    "user_model": userModel.toJson(),
+    "token": token,
+  };
+}
+
+class UserModel {
+  String id;
+  String name;
+  String email;
+  String phone;
+  String uid;
+  DateTime? createdAt;
+  DateTime? updatedAt;
+
+  UserModel({
+    required this.uid,
+    required this.id,
+    required this.name,
+    required this.email,
+    required this.phone,
+    this.createdAt,
+    this.updatedAt,
+  });
+
+  factory UserModel.fromRawJson(String str) => UserModel.fromJson(json.decode(str));
+
+  String toRawJson() => json.encode(toJson());
+
+  factory UserModel.fromJson(Map<String, dynamic> json) => UserModel(
+    id: json["id"],
+    name: json["name"],
+    email: json["email"],
+    phone: json["phone"],
+    createdAt: json["CreatedAt"] == null ? null : DateTime.parse(json["CreatedAt"]),
+    updatedAt: json["UpdatedAt"] == null ? null : DateTime.parse(json["UpdatedAt"]),
+    uid: json["uid"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "id": id,
+    "name": name,
+    "email": email,
+    "phone": phone,
+    "CreatedAt": createdAt?.toIso8601String(),
+    "UpdatedAt": updatedAt?.toIso8601String(),
+    "uid": uid,
+  };
+}

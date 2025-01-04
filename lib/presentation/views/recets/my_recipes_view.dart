9f76a37d7067bebd60e058b340533b676514724b
manuel castro barinas <manuel.castrobarinas@gmail.com>
2025-01-04 17:04:31 -0500

    update screens and view to get and change state to recipe

--- lib/presentation/views/recets/my_recipes_view.dart
+++ lib/presentation/views/recets/my_recipes_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:platosyplan/bloc/recipe/recipes_bloc.dart';
 import 'package:platosyplan/presentation/screens/recets/my_recipes_screen.dart';
 
 class MyRecipesView extends StatelessWidget {
@@ -12,7 +14,15 @@ class MyRecipesView extends StatelessWidget {
         leading   : IconButton(onPressed: () => Navigator.pop(context), icon: const Icon(Icons.arrow_back_ios_new_rounded)),
         title     : const Text('Mis Recetas', style: TextStyle(fontWeight: FontWeight.bold)),
       ),
-      body: const MyRecipesScreen()
+      body: FutureBuilder(
+        future  : BlocProvider.of<RecipesBloc>(context).getMyRecipes(), 
+        builder : (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) return const Center(child: CircularProgressIndicator());
+          if (snapshot.connectionState == ConnectionState.none)    return const Center(child: Text("No hay conexión"));
+          if (snapshot.hasError || snapshot.error == true)         return Center(child: Text(snapshot.error.toString()));
+          return const MyRecipesScreen();
+        }
+      )
     );
   }
 }
